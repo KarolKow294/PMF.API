a4197e0e58d6f0c7fc94eee42d4f9cb974a2f009
KarolKow294 <kowalski.karol294@gmail.com>
2024-03-11 18:52:39 +0100

    Entities

---
+++ PMF.API/Entities/Part.cs
@@ -0,0 +1,22 @@
+﻿namespace PMF.API.Entities
+{
+    public class Part
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Quantity { get; set; }
+        public string Material { get; set; }
+        public SurfaceType Surface { get; set; }
+        public IFormFile Drawing { get; set; }
+        public QuickResponseCode QrCode { get; set; }
+        public string ActualStorage { get; set; }
+        public string DestinationStorage { get; set; }
+    }
+
+    public enum SurfaceType
+    {
+        Painted,
+        Galvanised
+    }
+}
---
+++ PMF.API/Entities/QuickResponseCode.cs
@@ -0,0 +1,6 @@
+﻿namespace PMF.API.Entities
+{
+    public class QuickResponseCode
+    {
+    }
+}
